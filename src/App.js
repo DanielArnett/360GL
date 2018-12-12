dddadbc3b73c39c646fc623e61ec842b361b25bc
Eugene Fang <fang.eugene@gmail.com>
2018-12-12 15:57:19 -0500

    Slider with number

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Slider from '@material-ui/lab/Slider';
+import './App.css'
 
 class App extends Component {
+  state = {
+    value: 0,
+  }
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App-container'>
+        <div className='App-slider'>
+          <Slider
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className='App-map'>
+          {this.state.value}
+        </div>
       </div>
     );
   }
