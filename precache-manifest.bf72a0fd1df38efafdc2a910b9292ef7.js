945734ca5fd67adfaa6827ddb60d5bc185f7ce82
Daniel Arnett <arnett.daniel@gmail.com>
2018-12-28 09:23:51 -0500

    Updates

---
+++ precache-manifest.bf72a0fd1df38efafdc2a910b9292ef7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b10df561a0755f41edb",
+    "url": "/360GL/static/css/main.edee4bbd.chunk.css"
+  },
+  {
+    "revision": "8b10df561a0755f41edb",
+    "url": "/360GL/static/js/main.8b10df56.chunk.js"
+  },
+  {
+    "revision": "3f391b39648f12fc1f6f",
+    "url": "/360GL/static/js/1.3f391b39.chunk.js"
+  },
+  {
+    "revision": "9835fece5318bac9adb1",
+    "url": "/360GL/static/js/runtime~main.9835fece.js"
+  },
+  {
+    "revision": "2708308f3431eaeacdaac9fe5e39c262",
+    "url": "/360GL/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/360GL/precache-manifest.7e5ad0ccc9f6fc1d26b2f29040d8ad69.js"
+  "/360GL/precache-manifest.bf72a0fd1df38efafdc2a910b9292ef7.js"
 );
 
 workbox.clientsClaim();
